5da331885a9a419ed0835d12ebd5ee207df2483c
DESKTOP-N0219J1\moham <mohammad.sayyed@stud.fh-campuswien.ac.at>
2021-05-03 23:49:17 +0200

    partsolved

---
+++ src/main/java/downloader/ParallelDownloader.java
@@ -0,0 +1,13 @@
+package downloader;
+
+import downloader.Downloader;
+
+import java.util.List;
+
+public class ParallelDownloader extends Downloader {
+
+    @Override
+    public int process(List<String> urls) {
+        return 0;
+    }
+}
--- src/main/java/newsanalyzer/ctrl/Controller.java
+++ src/main/java/newsanalyzer/ctrl/Controller.java
@@ -1,24 +1,38 @@
 package newsanalyzer.ctrl;
 
+import downloader.SequentialDownloader;
 import newsapi.NewsApi;
 import newsapi.beans.Article;
 import newsapi.beans.NewsReponse;
 
+import javax.xml.crypto.Data;
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Collection;
 import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class Controller {
 
 	public static final String APIKEY = "a4a1aa5ca3644801bb74367e1447c738";
 
+
 	public void process(NewsApi newsApi) throws MalformedURLException, AnalyzerException {
 		System.out.println("Start process");
-
 
 		NewsReponse newsResponse = newsApi.getNews();
 		if(newsResponse != null) {
 			List<Article> articles = newsResponse.getArticles();
-			articles.stream().forEach(article -> System.out.println(article.toString()));
+			articles.forEach(article -> System.out.println(article.toString()));
+
+			List<String> urls = articles.stream()
+					.map(Article::getUrl)
+					.filter(Objects::nonNull)
+					.collect(Collectors.toList());
+			SequentialDownloader sequentialDownloader = new SequentialDownloader();
+			sequentialDownloader.process(urls);
 		}
 		//TODO implement Error handling
 
@@ -28,7 +42,9 @@ public class Controller {
 
 		System.out.println("End process");
 	}
-	
+
+
+
 
 	public Object getData() {
 		
